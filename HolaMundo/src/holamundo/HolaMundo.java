4f4f43d9ca8980ec0112f8ced0042e5f6e202f14
ivansanjuan <ivansanjuansilvestre@gmail.com>
2021-11-22 19:28:30 +0100

    He añadido un print con Hola Mundo

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -17,7 +17,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hola Mundo");
     }
 
 }
