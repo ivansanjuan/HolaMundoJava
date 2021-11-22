6f03e5d1082976fa73484847f004bf3b1d9405c9
a048607794p <a048607794p@S09-PC09>
2021-11-22 19:26:00 +0100

    Proyecto en blanco de Hola Mundo

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*
+ * Proyecto HolaMundo - Archivo HolaMundo.java - Compañía DAW
+ * Licencia Crative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package holamundo;
+
+/**
+ * @author Iván Sanjuán Silvestre <ivansanjuansilvestre@gmail.com>
+ * @version 1.0 
+ * @date 22 nov. 2021 19:08:51
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
